f9ffe02f3a6be19d3ee6beec956c51f3ccecc852
vkorotkikh <va.korotki@gmail.com>
2017-11-11 11:08:36 -0500

    New gui dev

---
+++ bc4cg_run.py
@@ -0,0 +1,17 @@
+# ******************************************************************************
+# Name:    [TBD]
+# Author:  Vadim Korotkikh
+# Email:   va.korotki@gmail.com
+# Date:    November 2017
+# Version: [TBD]
+#
+# Description: [Gui code?]
+#
+# ******************************************************************************
+# Library Imports
+
+import os
+
+
+def main():
+	pass
